3fe263306e5ecf4b5a6f1e5697ff717fd80f4eb5
Narphelflargle <philip.freidhoff@gmail.com>
2020-11-30 20:36:49 -0500

    Initial commit.

---
+++ apps/01_hello_world/program.py
@@ -0,0 +1,9 @@
+print('----------------------------------')
+print('            HELO APP')
+print('----------------------------------')
+print()
+
+user_text = input('What is you name? ')
+greeting = 'Nice to meet you ' + user_text
+
+print(greeting)
